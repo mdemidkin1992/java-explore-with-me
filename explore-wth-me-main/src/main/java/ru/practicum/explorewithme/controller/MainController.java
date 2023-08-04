0dafa24c09273136e84fd53d14ab5e3f4578ea05
mdemidkin <mdemidkin@yandex.ru>
2023-08-04 18:38:41 +0200

    clean: rm unused imports 1

--- explore-wth-me-main/src/main/java/ru/practicum/explorewithme/controller/MainController.java
+++ explore-wth-me-main/src/main/java/ru/practicum/explorewithme/controller/MainController.java
@@ -8,10 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.explorewithme.StatsClient;
-import ru.practicum.explorewithme.dto.StatDto;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
