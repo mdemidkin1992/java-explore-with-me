07ba2074032566004f38c654acf8f109e4fadbb8
mdemidkin <mdemidkin@yandex.ru>
2023-08-07 11:59:18 +0200

    fix: changed @Data to @Getter + @Setter in entity classes

--- explore-with-me-stats/stats-service/src/main/java/ru/practicum/explorewithme/model/Stat.java
+++ explore-with-me-stats/stats-service/src/main/java/ru/practicum/explorewithme/model/Stat.java
@@ -1,16 +1,14 @@
 package ru.practicum.explorewithme.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "stats")
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- explore-with-me-stats/stats-service/src/main/java/ru/practicum/explorewithme/model/Uri.java
+++ explore-with-me-stats/stats-service/src/main/java/ru/practicum/explorewithme/model/Uri.java
@@ -1,15 +1,13 @@
 package ru.practicum.explorewithme.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "uris")
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
