af399951bbdb29fdb3d71ce551b36e6df4d8714f
mdemidkin <mdemidkin@yandex.ru>
2023-08-17 13:30:14 +0200

    clean: rm unused imports 1

--- explore-with-me-stats/stats-service/src/test/java/ru/practicum/explorewithme/service/StatsServiceImplTest.java
+++ explore-with-me-stats/stats-service/src/test/java/ru/practicum/explorewithme/service/StatsServiceImplTest.java
@@ -14,13 +14,10 @@ import ru.practicum.explorewithme.repository.StatsRepository;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
